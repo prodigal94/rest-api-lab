d417446c602c93e0bc2b04dbbdcaae6898d137b6
prodigal94 <p06414040@proton.me>
2026-04-15 14:00:34 +0800

    Update api.php

--- adet-rest/routes/api.php
+++ adet-rest/routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Api\PrescriptionController;
 
 Route::apiResource('patients', PatientController::class);
 Route::apiResource('medicines', MedicineController::class);
-Route::apiResource('prescriptions', PatientController::class);
+Route::apiResource('prescriptions', PrescriptionController::class);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
